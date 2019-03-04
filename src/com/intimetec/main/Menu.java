78e4898c5b90e149f3b637f5a0fb53dc6c1b030a
Akshay Toshniwal <akshay.toshniwal@intimetec.com>
2019-03-04 10:19:42 +0530

    Implementation of jdbc

--- src/com/intimetec/main/Menu.java
+++ src/com/intimetec/main/Menu.java
@@ -1,5 +1,12 @@
 package com.intimetec.main;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Menu implements Comparable<Menu> {
 	String itemName;
 	boolean available;
@@ -11,26 +18,60 @@ public class Menu implements Comparable<Menu> {
 	}
 
 	public String toString() {
-		return itemName + " " + available;
+		return itemName + " ";
 	}
 
 	public boolean equals(Object o) {
-		Menu m = (Menu)o;
+		Menu m = (Menu) o;
 		return this.itemName.equalsIgnoreCase(m.itemName);
-			
+
 	}
-	public int hashCode()
-	{
-		return ((int)this.itemName.charAt(2))+((int)this.itemName.charAt(0))*2 +(this.itemName.charAt(1)*3);
+
+	public int hashCode() {
+
+		return ((int) this.itemName.charAt(0)) + ((int) this.itemName.charAt(1)) * 2 + (this.itemName.charAt(2) * 3);
 	}
-	
+
 	public int compareTo(Menu o) {
 		if ((this.itemName).compareTo(o.itemName) > 0) {
 			return 1;
-		}
-		else if ((this.itemName).compareTo(o.itemName) < 0)
+		} else if ((this.itemName).compareTo(o.itemName) < 0)
 			return -1;
 		else
 			return 0;
 	}
+
+	public static ArrayList<Menu> getMenuList() {
+		String selectQuery = "SELECT itemName,availability FROM Menu";
+		HashSet<Menu> menuItem = new HashSet<Menu>();
+		ArrayList<Menu> menuList = new ArrayList<Menu>();
+		Connection connection = DatabaseConnection.getConnection();
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		if (connection != null) {
+			try {
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery(selectQuery);
+				while (resultSet.next()) {
+					menuItem.add(new Menu(resultSet.getString(1), resultSet.getBoolean(2)));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					resultSet.close();
+					statement.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			System.out.println("connectivity issue");
+		}
+		menuList.addAll(menuItem);
+		return menuList;
+	}
+
 }
--- src/com/intimetec/main/RestaurentApp.java
+++ src/com/intimetec/main/RestaurentApp.java
@@ -2,7 +2,7 @@ package com.intimetec.main;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import com.intimetec.exceptionOfRestaurentapp.InvalidDataException;
@@ -15,26 +15,13 @@ public class RestaurentApp {
 		String userOrderList[] = null;
 		Scanner scan = new Scanner(System.in);
 
-		HashSet<Menu> menuItem = new HashSet<Menu>();
 		PrepareOrder prepareOrder = new PrepareOrder();
 		Kitchen kitchen = new Kitchen();
 
-		menuItem.add(new Menu("Paneer butter Masala", true)); // creating item of menu
-		menuItem.add(new Menu("Chana Masala", true));
-		menuItem.add(new Menu("Sev tamater", true));
-		menuItem.add(new Menu("Besan Gutta", true));
-		menuItem.add(new Menu("Cauliflower (Gobi)", false));
-		menuItem.add(new Menu("Malai Methi", true));
-		menuItem.add(new Menu("Aloo Pyaj Paraatha", true));
-		menuItem.add(new Menu("Gobi Paratha", false));
-		menuItem.add(new Menu("Tandoor Roti", true));
-		menuItem.add(new Menu("Tava Roti", true));
-
 		ArrayList<Menu> menuList = new ArrayList<Menu>();
-		menuList.addAll(menuItem);
-
+		menuList = Menu.getMenuList();
 		Collections.sort(menuList); // sorting the list in alphabetic order
-
+		
 		for (Menu item : menuList) { // print menu list on console with the availaible
 			System.out.print(menuList.indexOf(item) + "\t");
 			if (item.available) {
@@ -43,50 +30,50 @@ public class RestaurentApp {
 				System.out.println(item.itemName + "     Not Available\n");
 			}
 		}
+		if (!menuList.isEmpty()) {
+			do {
+				ArrayList<Menu> orderList = new ArrayList<Menu>();
+				int flag = 1;
 
-		do {
-			ArrayList<Menu> orderList = new ArrayList<Menu>();
-			int flag = 1;
+				System.out.println("\n Enter your order by serial no seperated by comma(,)");
+				userOrder = scan.next();// taking order in a string
+				userOrderList = userOrder.split(",");
 
-			System.out.println("\n Enter your order by serial no seperated by comma(,)"); 
-			userOrder = scan.next();// taking order in a string
-			userOrderList = userOrder.split(",");
+				try {
 
-			try {
+					for (int i = 0; i < userOrderList.length; i++) {
+						if (Integer.parseInt(userOrderList[i]) < menuList.size()) { // check first input data is valid or not
+							Menu m = menuList.get((int) userOrderList[i].charAt(0) - 48);
+							if (m.available) { // checking availability
+								orderList.add(m);
+							} else {
+								System.out.println(m.itemName + " is not available");
+								flag = 0; // change the flag status
+							}
+						}
 
-				for (int i = 0; i < userOrderList.length; i++) {
-					if (userOrderList[i].length() == 1) { // check first input data is valid or not
-						Menu m = menuList.get((int) userOrderList[i].charAt(0) - 48);
-						if (m.available) { // checking availability
-							orderList.add(m);
-						} else {
-							System.out.println(m.itemName + " is not available");
-							flag = 0; // change the flag status
+						else {
+							throw new InvalidDataException();
 						}
 					}
-
-					else {
-						throw new InvalidDataException();
-					}
+				} catch (InvalidDataException e) {
+					flag = 0;
+					System.out.println(e.getMessage());
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
 				}
-			} catch (InvalidDataException e) {
-				flag = 0;
-				System.out.println(e.getMessage());
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
 
-			if (flag != 0) // checking the flag status
-			{
-				System.out.println("your order is " + orderList); // print the order list
-				prepareOrder.add(orderList); // sending the order list to prepare the order object and add into the
-												// order queue
-				kitchen.notifyToChef();
-			}
-			orderList.clear(); // clearing the order list for next order
+				if (flag != 0) // checking the flag status
+				{
+					System.out.println("your order is " + orderList); // print the order list
+					prepareOrder.add(orderList); // sending the order list to prepare the order object and add into the
+													// order queue
+					kitchen.notifyToChef();
+				}
+				orderList.clear(); // clearing the order list for next order
 
-		} while (true);
-
+			} while (true);
+		}
 	}
 
 }
